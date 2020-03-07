f7c90845f62e6abeec367e1ccfd07f4f837db520
Rob Kitson <rkitson@evidation.com>
2020-03-06 16:24:51 -0800

    Forgot to commit a test

--- test/activerecord/archiver/store/s3/write_test.rb
+++ test/activerecord/archiver/store/s3/write_test.rb
@@ -42,4 +42,20 @@ class ActiveRecord::Archiver::Store::S3::WriteTest < Minitest::Test
 
     @s3.write(data, sub_path)
   end
+
+
+  def test_write_with_no_data
+
+    sub_path = 'sottotracciato'
+    full_path = File.join('percorso/completo/', sub_path)
+    data = "some data"
+
+    @s3.stubs(:make_path).with(sub_path).returns(full_path)
+    @s3.stubs(:format).with(data).returns(data)
+
+    response = stub(:successful? => true)
+    @s3.stubs(:send_data).with('secchio', full_path, data).returns(response)
+
+    @s3.write(data, sub_path)
+  end
 end
